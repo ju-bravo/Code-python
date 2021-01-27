15796f51a74945e2cf2b1e6cebfc382958156d7f
Juliana Bravo <ju-bravo@users.noreply.github.com>
2021-01-27 16:27:22 -0300

    Delete exe002.py

--- Curso em video/exe002.py
+++
@@ -1,5 +0,0 @@
-#  exe002.py --> Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas;
-------------------------------------------------------------------------------------------------------|
-
-nome = input("Olá, qual é o seu nome?")
-print("É um prazer te conhecer {}!".format(nome))
